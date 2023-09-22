04afad51de2e2753c48c6655dcc778408c8b0be8
Rahoul Baruah <rahoulb@echodek.co>
2023-09-22 17:56:26 +0100

    Performance boost

--- app/models/address_builder.rb
+++ app/models/address_builder.rb
@@ -12,6 +12,17 @@ class AddressBuilder
 
   def find_block_matching(address)
     address_ip = IPAddr.new address
+    pieces = address.split(".")
+    # Try to limit how many blocks we have to search through
+    three_pieces = [pieces[0], pieces[1], pieces[2]].join(".")
+    two_pieces = [pieces[0], pieces[1]].join(".")
+    one_piece = pieces[0]
+    [three_pieces, two_pieces, one_piece].each do |range|
+      puts range 
+      application[:blocks].where("range like ?", "#{range}%").find_each do |block|
+        return block if block.include? address_ip
+      end
+    end
     application[:blocks].find_each do |block|
       return block if block.include? address_ip
     end
